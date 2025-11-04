c8f2b081465c46c2470ecc5ac2623b477d4ecadc
nisalmissaka <wishwakanisal@gmail.com>
2025-11-04 11:15:31 +0530

    Created AccountHolderRepository and TransactionRepository interfaces

---
+++ src/main/java/edu/example/repository/AccountHolderRepository.java
@@ -0,0 +1,9 @@
+package edu.example.repository;
+
+import edu.example.model.AccountHolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountHolderRepository extends JpaRepository<AccountHolder,Long> {
+}
---
+++ src/main/java/edu/example/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package edu.example.repository;
+
+import edu.example.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+}
