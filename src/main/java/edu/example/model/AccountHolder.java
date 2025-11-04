de5d356ef4e3b65ad77ef5dbc60820bda959d0fb
nisalmissaka <wishwakanisal@gmail.com>
2025-11-04 09:58:53 +0530

    Added @Data annotation to AccountHolder model class

--- src/main/java/edu/example/model/AccountHolder.java
+++ src/main/java/edu/example/model/AccountHolder.java
@@ -1,7 +1,15 @@
 package edu.example.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
 public class AccountHolder {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accountId;
 
     private String accountHolderName;
